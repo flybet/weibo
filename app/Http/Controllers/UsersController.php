7b33598d2b7593ca228e974408b44360415d3e7b
hadson <vtaochao@msn.com>
2022-05-05 08:42:53 +0000

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,8 +8,18 @@ use Auth;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', ['show', 'create', 'store']);
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
+
         return view('users.create');
     }
 
@@ -40,21 +50,22 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(User $user, Request $request)
     {
+        $this->authorize('update', $user);
         $this->validate($request, [
             'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
         ]);
 
-        $data=[];
-        $data['name']=$request->name;
-        if($request->password)
-        {
-            $data['password']=$request->password;
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = $request->password;
         }
 
         $user->update($data);
@@ -63,8 +74,8 @@ class UsersController extends Controller
 //            'password' => bcrypt($request->password)
 //        ]);
 
-        session()->flash('success','个人资料更新成功');
+        session()->flash('success', '个人资料更新成功');
 
-        return redirect()->route('users.show',$user);
+        return redirect()->route('users.show', $user);
     }
 }
